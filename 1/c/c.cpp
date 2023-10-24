a3cb31de535a9f99620ff58db1c9ae406c6f8885
kismatov <kismatov@business-software.ru>
2023-10-24 14:34:19 +0400

    Тренировка 1

---
+++ 1/c/c.cpp
@@ -0,0 +1,179 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ln '\n'
+#define All(x) (x).begin(), (x).end()
+#define Allr(x) (x).rbegin(), (x).rend()
+#define Allf(x) x.begin() + 1, x.end()
+
+#define fi(a, b) for (auto i = (a); i <= (b); i++)
+#define fj(a, b) for (auto j = (a); j <= (b); j++)
+#define fo(a, b) for (auto o = (a); o <= (b); o++)
+#define fq(a, b) for (auto q = (a); q <= (b); q++)
+#define fe(a, b) for (auto e = (a); e <= (b); e++)
+#define fw(a, b) for (auto w = (a); w <= (b); w++)
+
+#define fdi(a, b) for (auto i = (a); i >= (b); i--)
+#define fdj(a, b) for (auto j = (a); j >= (b); j--)
+#define fdo(a, b) for (auto o = (a); o >= (b); o--)
+#define fdq(a, b) for (auto q = (a); q >= (b); q--)
+#define fde(a, b) for (auto e = (a); e >= (b); e--)
+#define fdw(a, b) for (auto w = (a); w >= (b); w--)
+
+#define fx(A) for (auto &x : (A))
+#define fy(A) for (auto &y : (A))
+#define fz(A) for (auto &z : (A))
+
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define sz(x) (int)x.size()
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cmath>
+#include <cstdarg>
+#include <cassert>
+#include <ctime>
+#include <tuple>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+#ifdef _DEBUG
+#define LOCAL
+#endif
+
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+
+typedef vector<vb> vvb;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vc> vvc;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename A, typename B>
+ostream& operator<<(ostream& os, pair<A, B> p) {
+	os << "(" << p.first << ", " << p.second << ")";
+	return os;
+}
+ 
+template<typename T>
+ostream& operator<<(ostream& os, vector<T> v) {
+	fi(0, sz(v) - 1) {
+		os << v[i] << " ";
+	}
+	return os;
+}
+ 
+template<typename T>
+ostream& operator<<(ostream& os, set<T> t) {
+	for (auto z : t) {
+		os << z << " ";
+	}
+	return os;
+}
+ 
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, map<T1, T2> t) {
+	cerr << endl;
+	for (auto z : t) {
+		os << "\t" << z.first << " -> " << z.second << endl;
+	}
+	return os;
+}
+ 
+#ifdef LOCAL
+#define dbg(x) {cerr << __LINE__ << "\t" << #x << ": " << (x) << endl;}
+#else
+#define dbg(x) {}
+#endif
+ 
+#ifdef LOCAL
+#define ass(x) if (!(x)) { cerr << __LINE__ << "\tassertion failed: " << #x << endl, abort(); }
+#else
+#define ass(x) assert(x)
+#endif
+
+ll n;
+vll a;
+vll d;
+vll p;
+
+void solve() {
+	d[1] = 1;
+
+	ll m = 1;
+	ll b = 1;
+
+	fi(2, n) {
+		fj(0, i - 1) {
+			if(a[j] < a[i]) {
+				if(d[j] + 1 > d[i]) {
+					d[i] = d[j] + 1;
+					p[i] = j;
+					if(d[i] > m) {
+						m = d[i];
+						b = i;
+					}
+				}
+			}
+		}
+	}
+	vll ans;
+	int i = b;
+	while(i != 0) {
+		ans.pb(a[i]);
+		i = p[i];
+	}
+	reverse(All(ans));
+	cout << d[b] << ln;
+	fz(ans) cout << z << ' ';
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+	cin >> n;
+	a = vll(n + 1);
+	d = vll(n + 1);
+	p = vll(n + 1);
+	fi(1, n) cin >> a[i];
+
+	solve();
+}
---
+++ 1/d/d.cpp
@@ -0,0 +1,174 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ln '\n'
+#define All(x) (x).begin(), (x).end()
+#define Allr(x) (x).rbegin(), (x).rend()
+#define Allf(x) x.begin() + 1, x.end()
+
+#define fi(a, b) for (auto i = (a); i <= (b); i++)
+#define fj(a, b) for (auto j = (a); j <= (b); j++)
+#define fo(a, b) for (auto o = (a); o <= (b); o++)
+#define fq(a, b) for (auto q = (a); q <= (b); q++)
+#define fe(a, b) for (auto e = (a); e <= (b); e++)
+#define fw(a, b) for (auto w = (a); w <= (b); w++)
+
+#define fdi(a, b) for (auto i = (a); i >= (b); i--)
+#define fdj(a, b) for (auto j = (a); j >= (b); j--)
+#define fdo(a, b) for (auto o = (a); o >= (b); o--)
+#define fdq(a, b) for (auto q = (a); q >= (b); q--)
+#define fde(a, b) for (auto e = (a); e >= (b); e--)
+#define fdw(a, b) for (auto w = (a); w >= (b); w--)
+
+#define fx(A) for (auto &x : (A))
+#define fy(A) for (auto &y : (A))
+#define fz(A) for (auto &z : (A))
+
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define sz(x) (int)x.size()
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cmath>
+#include <cstdarg>
+#include <cassert>
+#include <ctime>
+#include <tuple>
+#include <unordered_set>
+#include <unordered_map>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+#ifdef _DEBUG
+#define LOCAL
+#endif
+
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vd;
+typedef vector<char> vc;
+typedef vector<string> vs;
+
+typedef vector<vb> vvb;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vc> vvc;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename A, typename B>
+ostream& operator<<(ostream& os, pair<A, B> p) {
+	os << "(" << p.first << ", " << p.second << ")";
+	return os;
+}
+ 
+template<typename T>
+ostream& operator<<(ostream& os, vector<T> v) {
+	fi(0, sz(v) - 1) {
+		os << v[i] << " ";
+	}
+	return os;
+}
+ 
+template<typename T>
+ostream& operator<<(ostream& os, set<T> t) {
+	for (auto z : t) {
+		os << z << " ";
+	}
+	return os;
+}
+ 
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, map<T1, T2> t) {
+	cerr << endl;
+	for (auto z : t) {
+		os << "\t" << z.first << " -> " << z.second << endl;
+	}
+	return os;
+}
+ 
+#ifdef LOCAL
+#define dbg(x) {cerr << __LINE__ << "\t" << #x << ": " << (x) << endl;}
+#else
+#define dbg(x) {}
+#endif
+ 
+#ifdef LOCAL
+#define ass(x) if (!(x)) { cerr << __LINE__ << "\tassertion failed: " << #x << endl, abort(); }
+#else
+#define ass(x) assert(x)
+#endif
+
+ld eps = 1e-9;
+
+struct Point {
+	ll x, y;
+};
+
+
+Point A, B, C, D;
+
+ll double_area(Point a, Point b, Point c) {
+	ll res = abs(a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y);
+	return res;
+}
+
+void solve() {
+	ll s = double_area(A, B, C);
+	ll s1 = double_area(A, B, D);
+	ll s2 = double_area(A, C, D);
+	ll s3 = double_area(C, B, D);
+
+	if(s == s1 + s2 + s3) {
+		cout << "In" << ln;
+	} else {
+		cout << "Out" << ln;
+	}
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+	
+	ll x, y;
+	cin >> x >> y;
+	A = {x, y};
+	cin >> x >> y;
+	B = {x, y};
+	cin >> x >> y;
+	C = {x, y};
+	cin >> x >> y;
+	D = {x, y};
+
+	solve();
+}
